d73f55a904c8085d1f4f62bc310febfca48699f6
mohamed hassan <161214278+m-hassan-sarri@users.noreply.github.com>
2024-04-30 14:46:02 +0300

    dsadsad dsad sa dsa d

---
+++ app/home/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div className='bg-orange-500 text-cyan-400 text-center flex justify-center items-center h-screen'>
+      HomePage
+    </div>
+  )
+}
